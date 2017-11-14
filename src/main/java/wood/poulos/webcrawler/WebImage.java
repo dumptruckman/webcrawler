f84e0a01c0e3c41c823a1ca6e02e45275fc58cf7
Jeremy Wood <farachan@gmail.com>
2017-11-14 10:40:23 -0500

    Renamed ElementCopier to URLDownloader.

--- src/main/java/wood/poulos/webcrawler/WebImage.java
+++ src/main/java/wood/poulos/webcrawler/WebImage.java
@@ -1,11 +1,6 @@
 package wood.poulos.webcrawler;
 
-import org.jetbrains.annotations.NotNull;
-import wood.poulos.webcrawler.util.ElementCopier;
-
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Path;
 
 /**
  * A abstract representation of a image on the web.
