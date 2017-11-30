d6c5073cd02e0d8a4c6a98030806e84f243d9c5c
Jeremy Wood <farachan@gmail.com>
2017-11-29 22:57:28 -0500

    Fixed TestPrintStream for logging output.

--- src/test/java/wood/poulos/webcrawler/util/TestPrintStream.java
+++ src/test/java/wood/poulos/webcrawler/util/TestPrintStream.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 public class TestPrintStream extends PrintStream {
 
@@ -30,5 +31,45 @@ public class TestPrintStream extends PrintStream {
     @Override
     public String toString() {
         return out.toString();
+    }
+
+    public String getLastString() {
+        String outString = out.toString();
+        out.reset();
+
+        String[] lines = outString.split(System.getProperty("line.separator"));
+
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = extractMessage(lines[i]);
+        }
+
+        return joinLines(lines);
+    }
+
+    private String extractMessage(String s) {
+        String[] split;
+        if (s.startsWith("[")) {
+            // customized logger
+            split = s.split(":", 2);
+        } else {
+            split = s.split("-", 2);
+        }
+        if (split.length != 2) {
+            throw new IllegalStateException("The output does not appear to come from the logback logger.");
+        }
+
+        String message = split[1];
+        return message.trim();
+    }
+
+    private String joinLines(String[] lines) {
+        StringBuilder b = new StringBuilder();
+        for (String line : lines) {
+//            if (b.length() != 0) {
+//                b.append(System.getProperty("line.separator"));
+//            }
+            b.append(line).append(System.getProperty("line.separator"));
+        }
+        return b.toString();
     }
 }
